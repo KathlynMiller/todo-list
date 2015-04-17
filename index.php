31acf2adf05f6656d4c7bd0d6f815ce62b596b06
Kathlyn Miller <katkit101999@hotmail.com>
2015-04-17 12:06:11 -0700

    watched video 5, 5.1 and 6 coded and commented

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kathlyn's To-Do List</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+  <div class="warp">
+    <div class="task-list">
+    <ul>
+      <?php require("include/connect.php"); ?>
+    </ul>
+  </div>
+  <form class="add-new-task" autocomplete="off">
+   <input type="text" name="new-task" placeholder="Add new item..."/>
+   </form>
+   </div> 
+</body>
+</html>

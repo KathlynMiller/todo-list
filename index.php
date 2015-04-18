0d89676e06c45bc150a090eca301cb6bbf18fa4a
Kathlyn Miller <katkit101999@hotmail.com>
2015-04-17 18:11:27 -0700

    watched video 11 coded and commented

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 
       if(new_task != ''){
         $.post('includes/add-task.php',{ task: new_task}, function(data){
-           $(('add-newtask input[name=new-task]'].val();
+           $('add-newtask input[name=new-task]'].val();
             $(data).appendTo('task-list ul').hide().fadeIn();
          });
         }
